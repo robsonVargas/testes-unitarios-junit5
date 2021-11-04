28ae43ac363f2a852c52403c836d3344b6813d8d
Robson Vargas <robsonsvargas1993@gmail.com>
2021-11-04 18:16:57 -0300

    refatorando o código e testando novamente

--- src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
+++ src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
@@ -8,14 +8,18 @@ import br.com.treinaweb.twbiblioteca.models.Emprestimo;
 import br.com.treinaweb.twbiblioteca.models.Obra;
 
 public class EmprestimoService {
+
+    private static final int DIAS_PARA_DEVOLUCAO = 3;
     
     public Emprestimo novo(Cliente cliente, List<Obra> obras) {
         var emprestimo = new Emprestimo();
 
+        var dataEmprestimo = LocalDate.now();
+        var dataDevolocao = dataEmprestimo.plusDays(DIAS_PARA_DEVOLUCAO);
         emprestimo.setCliente(cliente);
         emprestimo.setLivros(obras);
-        emprestimo.setDataEmprestimo(LocalDate.now());
-        emprestimo.setDataDevolucao(LocalDate.now().plusDays(3));
+        emprestimo.setDataEmprestimo(dataEmprestimo);
+        emprestimo.setDataDevolucao(dataDevolocao);
 
         return emprestimo;
     }
