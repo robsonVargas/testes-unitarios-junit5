efc3316fc6da03a17cc9060b79ef72cb6c7a3597
Robson Vargas <robsonsvargas1993@gmail.com>
2021-11-04 17:09:41 -0300

    melhorando a declaração do teste

--- src/test/java/br/com/treinaweb/twbiblioteca/models/AutorTest.java
+++ src/test/java/br/com/treinaweb/twbiblioteca/models/AutorTest.java
@@ -1,16 +1,36 @@
 package br.com.treinaweb.twbiblioteca.models;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AutorTest {
 
     @Test
-    public void testaMetodoEstaVivo() {
+    public void quandoMetodoEstaVivoForChamadoComDataFalecimentoNulaDeveRetornarTrue() {
+        // cenário
         var autor = new Autor();
 
+        // execuçao
         var estaVivo = autor.estaVivo();
 
-        Assertions.assertEquals(true, estaVivo);
+        // verificação
+        assertEquals(true, estaVivo);
+    }
+
+    @Test
+    public void quandoMetodoEstaVivoForChamadoComDataFalecimentoPreenchidaDeveRetornarFalse() {
+        // cenario
+        var autor = new Autor();
+        autor.setDataFalecimento(LocalDate.now());
+
+        // execução
+        var estaVivo = autor.estaVivo();
+
+        // verificação
+        assertEquals(false, estaVivo);
     }
 }
