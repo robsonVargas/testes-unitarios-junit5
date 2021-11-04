c0d5d600715b88506e0331a324fe804cf80aabb8
Robson Vargas <robsonsvargas1993@gmail.com>
2021-11-04 19:17:41 -0300

    refatoração do método Emprestimo

--- src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
+++ src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
@@ -14,15 +14,7 @@ public class EmprestimoService {
         var emprestimo = new Emprestimo();
 
         var dataEmprestimo = LocalDate.now();
-        var diasParaDevolucao = 3;
-        if (cliente.getReputacao() == Reputacao.RUIM) {
-            diasParaDevolucao = 1;
-        } else if (cliente.getReputacao() == Reputacao.REGULAR) {
-            diasParaDevolucao = 3;
-        } else {
-            diasParaDevolucao = 5;
-        }
-
+        var diasParaDevolucao = cliente.getReputacao().obterDiasParaDevolucao();
         var dataDevolocao = dataEmprestimo.plusDays(diasParaDevolucao);
         
         emprestimo.setCliente(cliente);
